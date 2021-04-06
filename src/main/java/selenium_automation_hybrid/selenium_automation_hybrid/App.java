b18389e3b9ec88e55fa8aba74a364ce99ecc35f2
Administrate <unit.manikanta@gmail.com>
2021-04-06 20:19:08 +0530

    Maven project

---
+++ src/main/java/selenium_automation_hybrid/selenium_automation_hybrid/App.java
@@ -0,0 +1,13 @@
+package selenium_automation_hybrid.selenium_automation_hybrid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
